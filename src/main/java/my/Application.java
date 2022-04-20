ca5877559d96543d999d0a40f6e7056690562d8f
刘汪涛 <1372342410@qq.com>
2022-04-20 15:47:53 +0800

    22

--- src/main/java/my/Application.java
+++ src/main/java/my/Application.java
@@ -3,6 +3,7 @@ package my;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @MapperScan("my.mapper")
 @SpringBootApplication
